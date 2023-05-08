3b74e09bf2abae60b5b997c7c47bc0a775ac75e6
confidentmeerkat <confidentmeerkat@gmail.com>
2023-05-08 13:51:25 -0500

    orders actions

--- lib/components/customer-orders.tsx
+++ lib/components/customer-orders.tsx
@@ -7,14 +7,24 @@ import { useMemo, useState } from "react";
 import Add from "@mui/icons-material/Add";
 import dayjs from "dayjs";
 import OrderModal from "./order-modal";
+import { Delete, Visibility } from "@mui/icons-material";
+import Link from "next/link";
+import Axios from "axios";
+import { mutate } from "swr";
 
 export default function CustomerOrders({ data }: { data: Customer }) {
   const [modalOpen, setModalOpen] = useState(false);
 
+  const handleDelete = (id: string) => async () => {
+    await Axios.delete(`/api/orders/${id}`);
+
+    mutate(["/api/customers", data.id]);
+  };
+
   const columns: GridColDef[] = useMemo(() => {
     return [
       {
-        field: "id",
+        field: "_id",
         headerName: "No",
         filterable: false,
         renderCell: (index) => index.api.getRowIndexRelativeToVisibleRows(index.row.id) + 1,
@@ -41,6 +51,25 @@ export default function CustomerOrders({ data }: { data: Customer }) {
         headerName: "Date Completed",
         renderCell: ({ value }) => {
           return <Typography>{dayjs(value).format("MM/DD/YYYY")}</Typography>;
+        },
+      },
+      {
+        field: "id",
+        headerName: "Action",
+        sortable: false,
+        headerAlign: "center",
+        renderCell: ({ value }) => {
+          return (
+            <Stack direction="row" justifyContent="center">
+              <IconButton component={Link} href={`/orders/${value}`}>
+                <Visibility />
+              </IconButton>
+
+              <IconButton onClick={handleDelete(value)}>
+                <Delete />
+              </IconButton>
+            </Stack>
+          );
         },
       },
     ];
